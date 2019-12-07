22caf654bccefe01e372a3f21dbcc8155149093d
elsid <elsid.mail@gmail.com>
2019-12-07 12:38:45 +0100

    Mark rect methods inline

--- src/rect.rs
+++ src/rect.rs
@@ -1,4 +1,4 @@
-use crate::my_strategy::vec2::Vec2;
+use crate::my_strategy::Vec2;
 
 #[derive(Default, Clone, Debug)]
 pub struct Rect {
@@ -7,18 +7,22 @@ pub struct Rect {
 }
 
 impl Rect {
+    #[inline(always)]
     pub const fn new(center: Vec2, half: Vec2) -> Self {
         Self { center, half }
     }
 
+    #[inline(always)]
     pub const fn center(&self) -> Vec2 {
         self.center
     }
 
+    #[inline(always)]
     pub const fn half(&self) -> Vec2 {
         self.half
     }
 
+    #[inline(always)]
     pub fn collide(&self, other: &Self) -> Vec2 {
         Vec2::new(
             (self.center.x() - other.center.x()).abs() - (self.half.x() + other.half.x()),
@@ -26,14 +30,17 @@ impl Rect {
         )
     }
 
+    #[inline(always)]
     pub fn min(&self) -> Vec2 {
         self.center - self.half
     }
 
+    #[inline(always)]
     pub fn max(&self) -> Vec2 {
         self.center + self.half
     }
 
+    #[inline(always)]
     pub fn has_collision(&self, other: &Self) -> bool {
         self.min().x() < other.max().x()
             && self.max().x() > other.min().x()
