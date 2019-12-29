0489aa59ac6176f5ecb7e199024584804db7a179
elsid <elsid.mail@gmail.com>
2019-12-29 17:58:31 +0100

    Support place indendepdent runs

--- scripts/results_stats.py
+++ scripts/results_stats.py
@@ -5,130 +5,236 @@ import numpy
 import os.path
 import statistics
 import sys
+import operator
+import functools
+
+from collections import defaultdict, Counter
 
 
 def main():
-    import matplotlib.pyplot
-
     paths = sorted(sys.argv[1:], key=get_time)
     games = list(collect_data(paths))
-    players = dict(
-        first=dict(places=[], scores=[], score_diffs=[]),
-        second=dict(places=[], scores=[], score_diffs=[]),
-    )
-    for r in games:
-        for k, p in players.items():
-            p['places'].append(r[k]['place'])
-            p['scores'].append(r[k]['score'])
-        players['first']['score_diffs'].append(r['first']['score'] - r['second']['score'])
-        players['second']['score_diffs'].append(r['second']['score'] - r['first']['score'])
-    _1 = [sum(w == 1 for w in v['places']) for v in players.values()]
-    _2 = [sum(w == 2 for w in v['places']) for v in players.values()]
-    for v in players.values():
-        v['places'] = numpy.array(v['places'])
-        v['scores'] = numpy.array(v['scores'])
-    scores_partial_sums = [numpy.cumsum(v['scores']) for v in players.values()]
-    place_1_partial_sums = [numpy.cumsum([w == 1 for w in v['places']]) for v in players.values()]
-    stats = dict(
-        _1=_1,
-        _2=_2,
-        wins=[
-            sum(w[0] > w[1] for w in zip(*[v['scores'] for v in players.values()])),
-            sum(w[1] > w[0] for w in zip(*[v['scores'] for v in players.values()]))
-        ],
-        total_score=[sum(v['scores']) for v in players.values()],
-        min_score=[min(v['scores']) for v in players.values()],
-        max_score=[max(v['scores']) for v in players.values()],
-        mean_score=[statistics.mean(v['scores']) for v in players.values()],
-        median_score=[statistics.median(v['scores']) for v in players.values()],
-        stdev_score=[statistics.stdev(v['scores']) for v in players.values()],
-        q95_score=[numpy.quantile(v['scores'], 0.95) for v in players.values()],
-        min_score_diff=[min(v['score_diffs']) for v in players.values()],
-        max_score_diff=[max(v['score_diffs']) for v in players.values()],
-        mean_score_diff=[statistics.mean(v['score_diffs']) for v in players.values()],
-        median_score_diff=[statistics.median(v['score_diffs']) for v in players.values()],
-        stdev_score_diff=[statistics.stdev(v['score_diffs']) for v in players.values()],
-    )
-    draws = sum(w[0] == w[1] for w in zip(*[v['scores'] for v in players.values()]))
-    row('games', len(games))
-    row('second wins', stats['wins'][1], stats['wins'][1] / len(games))
-    row('draws', draws, draws / len(games))
-    row('first wins', stats['wins'][0], stats['wins'][0] / len(games))
+    stats = get_stats(games)
+    print_stats(stats)
+    show_stats(stats)
+
+
+def print_stats(stats):
+    row('metric', 'value', '%')
+    for metric in ('games', 'draws', 'zero_draws', 'unique_seeds'):
+        row(metric, stats[metric], stats[metric] / stats['games'] * 100)
     print()
-    row('', *(list(players.keys()) + ['ratio (second/first)']))
-    for k, v in stats.items():
-        row(k, *(v + [ratio(v)]))
-    print()
-    fig, ax = matplotlib.pyplot.subplots()
-    ax.set_title('scores distribution')
-    bins = numpy.linspace(0, max(max(v['scores']) for v in players.values()) + 1)
-    for k, v in players.items():
-        ax.hist(v['scores'], bins=bins, label=k, alpha=0.5)
+    players = stats['players']
+    row('metric', *players, *['%s %%' % v for v in players], '%s / %s' % (players[0], players[1]), '%s / %s' % (players[1], players[0]), 'total')
+    for metric, values in stats.items():
+        print_metric(metric, values, players)
+
+
+def print_metric(metric, values, players):
+    if isinstance(values, dict):
+        if values:
+            if isinstance(tuple(values.values())[0], dict):
+                for submetric, subvalues in values.items():
+                    print_metric('%s %s' % (metric, submetric), subvalues, players)
+            if isinstance(tuple(values.values())[0], (int, float)):
+                print_counter(metric, values, players)
+
+
+def print_counter(metric, values, players):
+    row_values = [values[v] for v in players]
+    total = sum(row_values)
+    fractions = [safe_div(v, total) * 100 for v in row_values]
+    ratios = [safe_div(values[players[0]], values[players[1]]), safe_div(values[players[1]], values[players[0]])]
+    row(metric, *row_values, *fractions, *ratios, total)
+
+
+def safe_div(a, b):
+    return a / b if b else float('inf')
+
+
+def show_stats(stats):
+    import matplotlib.pyplot as pyplot
+
+    show_metric_plot(pyplot, stats, 'scores_dynamic')
+    show_metric_plot(pyplot, stats, 'scores_dynamic_cumsum')
+    show_metric_plot(pyplot, stats, 'places_dynamic_cumsum')
+    show_metric_plot(pyplot, stats, 'wins_dynamic_cumsum')
+    show_metric_plot(pyplot, stats, 'losses_dynamic_cumsum')
+
+    show_ratio_plots(pyplot, stats, 'scores_dynamic_cumsum')
+    show_ratio_plots(pyplot, stats, 'places_dynamic_cumsum')
+    show_ratio_plots(pyplot, stats, 'wins_dynamic_cumsum')
+    show_ratio_plots(pyplot, stats, 'losses_dynamic_cumsum')
+
+    show_scores_distribution_plot(pyplot, stats)
+    show_positions_distribution_plot(pyplot, stats)
+    show_seeds_distribution_plot(pyplot, stats)
+
+    pyplot.show()
+
+
+def show_ratio_plots(pyplot, stats, metric):
+    players = stats['players']
+    show_plot(
+        pyplot,
+        name='%s %s / %s' % (metric, players[0], players[1]),
+        values=stats[metric][players[0]] / stats[metric][players[1]],
+    )
+    show_plot(
+        pyplot,
+        name='%s %s / %s' % (metric, players[1], players[0]),
+        values=stats[metric][players[1]] / stats[metric][players[0]],
+    )
+
+
+def show_metric_plot(pyplot, stats, metric):
+    fig, ax = pyplot.subplots()
+    fig.canvas.set_window_title(metric)
+    ax.set_title(metric)
+    for player, values in stats[metric].items():
+        ax.plot(numpy.arange(0, len(values), 1), values, label=player)
+    total = functools.reduce(operator.add, stats[metric].values())
+    ax.plot(numpy.arange(0, len(total), 1), total, label='total')
+    ax.grid(True)
+    ax.legend()
+
+
+def show_plot(pyplot, name, values):
+    fig, ax = pyplot.subplots()
+    fig.canvas.set_window_title(name)
+    ax.set_title(name)
+    ax.plot(numpy.arange(0, len(values), 1), values, label=name)
+    ax.grid(True)
+    ax.legend()
+
+
+def show_scores_distribution_plot(pyplot, stats):
+    players = stats['players']
+    fig, ax = pyplot.subplots()
+    fig.canvas.set_window_title('scores_dynamic')
+    ax.set_title('scores_dynamic')
+    bins = numpy.linspace(0, max(stats['max_score'][v] for v in players) + 1, 50)
+    for player, values in stats['scores_dynamic'].items():
+        ax.hist(values, bins=bins, label=player, alpha=0.5)
         ax.set_xticks(bins)
         ax.grid(True)
         ax.legend()
-    _1 = {k: [0] for k in players.keys()}
-    fig, ax = matplotlib.pyplot.subplots()
-    ax.set_title('place 1 dynamic')
-    for g in games:
-        for k, v in _1.items():
-            v.append(v[-1] + (g[k]['place'] == 1))
-    for k, v in _1.items():
-        ax.plot(numpy.arange(0, len(games) + 1, 1), v, label=k)
-        ax.grid(True)
-        ax.legend()
-    scores = {k: [0] for k in players.keys()}
-    fig, ax = matplotlib.pyplot.subplots()
-    ax.set_title('scores dynamic')
-    for g in games:
-        for k, v in scores.items():
-            v.append(v[-1] + g[k]['score'])
-    for k, v in scores.items():
-        ax.plot(numpy.arange(0, len(games) + 1, 1), v, label=k)
-        ax.grid(True)
-        ax.legend()
-    fig, ax = matplotlib.pyplot.subplots()
-    ax.set_title('scores diffs')
-    bins = numpy.linspace(
-        min(min(v['score_diffs']) for v in players.values()),
-        max(max(v['score_diffs']) for v in players.values()) + 1
-    )
-    for k, v in players.items():
-        ax.hist(v['score_diffs'], bins=50, label=k, alpha=0.5)
+
+
+def show_positions_distribution_plot(pyplot, stats):
+    players = stats['players']
+    fig, ax = pyplot.subplots()
+    fig.canvas.set_window_title('positions_dynamic')
+    ax.set_title('positions_dynamic')
+    bins = [0, 1, 2]
+    for player, values in stats['positions_dynamic'].items():
+        ax.hist(values, bins=bins, label=player, alpha=0.5)
         ax.set_xticks(bins)
         ax.grid(True)
         ax.legend()
-    fig, ax = matplotlib.pyplot.subplots()
-    seeds = numpy.array([v['seed'] for v in games])
-    ax.hist(seeds)
-    fig, ax = matplotlib.pyplot.subplots()
-    ax.set_title('scores ratio dynamic first / second')
-    scores_partial_sums_ratio = numpy.array([v[0] / v[1] for v in zip(*scores_partial_sums)])
-    ax.plot(numpy.arange(0, len(games), 1), scores_partial_sums_ratio)
+
+
+def show_seeds_distribution_plot(pyplot, stats):
+    fig, ax = pyplot.subplots()
+    fig.canvas.set_window_title('seeds')
+    ax.set_title('seeds')
+    bins = numpy.linspace(0, 2**64, 32)
+    ax.hist(stats['seeds'], bins=32)
+    ax.set_xticks(bins)
     ax.grid(True)
-    fig, ax = matplotlib.pyplot.subplots()
-    ax.set_title('place 1 ratio dynamic second / first')
-    place_1_partial_sums_ratio = numpy.array([v[1] / (v[0] or 1) for v in zip(*place_1_partial_sums)])
-    ax.plot(numpy.arange(0, len(games), 1), place_1_partial_sums_ratio)
-    ax.grid(True)
-    matplotlib.pyplot.show()
+
+
+def get_stats(games):
+    draws = 0
+    zero_draws = 0
+    players = set()
+    wins = Counter()
+    losses = Counter()
+    places = defaultdict(Counter)
+    crashes = Counter()
+    positions = defaultdict(Counter)
+    places_positions = defaultdict(lambda: defaultdict(Counter))
+    seeds = set()
+    scores = defaultdict(list)
+    places_dynamic = defaultdict(list)
+    positions_dynamic = defaultdict(list)
+    wins_dynamic = defaultdict(list)
+    losses_dynamic = defaultdict(list)
+    for number, game in enumerate(games):
+        game_scores = numpy.array(sorted(frozenset(v['score'] for v in game['results'].values()), reverse=True))
+        if len(game_scores) == 1:
+            draws += 1
+            if game_scores[0] == 0:
+                zero_draws += 1
+        max_score = max(game_scores)
+        min_score = min(game_scores)
+        if 1 == sum(1 for v in game_scores if v == max_score):
+            winner = next(k for k, v in game['results'].items() if v['score'] == max_score)
+            wins[winner] += 1
+            wins_dynamic[winner].append(1)
+        if 1 == sum(1 for v in game_scores if v == min_score):
+            loser = next(k for k, v in game['results'].items() if v['score'] == min_score)
+            losses[loser] += 1
+            losses_dynamic[loser].append(1)
+        for place, score in enumerate(game_scores):
+            for k, v in game['results'].items():
+                if v['score'] == score:
+                    places[place + 1][k] += 1
+                    places_dynamic[k].append(place + 1)
+                    places_positions[place + 1][v['position']][k] += 1
+        for k, v in game['results'].items():
+            players.add(k)
+            if v['crashed']:
+                crashes[k] += 1
+            scores[k].append(v['score'])
+            positions[v['position']][k] += 1
+            positions_dynamic[k].append(v['position'])
+            if len(wins_dynamic[k]) < number + 1:
+                wins_dynamic[k].append(0)
+            if len(losses_dynamic[k]) < number + 1:
+                losses_dynamic[k].append(0)
+        seeds.add(game['seed'])
+    for k in scores.keys():
+        scores[k] = numpy.array(scores[k])
+        places_dynamic[k] = numpy.array(places_dynamic[k])
+    return dict(
+        games=len(games),
+        draws=draws,
+        zero_draws=zero_draws,
+        unique_seeds=len(seeds),
+        players=sorted(players),
+        wins=wins,
+        losses=losses,
+        places=places,
+        crashes=crashes,
+        positions=positions,
+        places_positions=places_positions,
+        total_score={k: sum(v) for k, v in scores.items()},
+        median_score={k: statistics.median(v) for k, v in scores.items()},
+        mean_score={k: statistics.mean(v) for k, v in scores.items()},
+        stdev_score={k: statistics.stdev(v) for k, v in scores.items()},
+        min_score={k: min(v) for k, v in scores.items()},
+        max_score={k: max(v) for k, v in scores.items()},
+        q95_score={k: numpy.quantile(v, 0.95) for k, v in scores.items()},
+        scores_dynamic=scores,
+        scores_dynamic_cumsum=cumsums(scores),
+        places_dynamic=places_dynamic,
+        places_dynamic_cumsum=cumsums(places_dynamic),
+        wins_dynamic=wins_dynamic,
+        wins_dynamic_cumsum=cumsums(wins_dynamic),
+        losses_dynamic=losses_dynamic,
+        losses_dynamic_cumsum=cumsums(losses_dynamic),
+        positions_dynamic=positions_dynamic,
+        seeds=numpy.array(sorted(seeds)),
+    )
+
+
+def cumsums(values):
+    return {k: numpy.cumsum(v) for k, v in values.items()}
 
 
 def get_time(path):
-    components = os.path.basename(path).split('.')
-    if len(components) == 6:
-        return int(components[-3]) * 1e6 + int(components[-2])
-    else:
-        return int(components[-2]) * 1e6
-
-
-def ratio(values):
-    if values[1] == values[0]:
-        return 1
-    elif values[0] == 0:
-        return float('inf')
-    else:
-        return values[1] / values[0]
+    return int(os.path.basename(path).split('.')[-1])
 
 
 def row(*args):
@@ -137,26 +243,54 @@ def row(*args):
 
 def collect_data(paths):
     for path in paths:
-        content = read_result(path)
-        if content:
-            yield parse_result(content)
+        config_path = os.path.join(path, 'config.json')
+        if not os.path.exists(config_path):
+            continue
+        config_content = read_file(config_path)
+        if not config_content:
+            continue
+        players = parse_config(config_content)
+        result_path = os.path.join(path, 'result.json')
+        if not os.path.exists(result_path):
+            continue
+        result_content = read_file(result_path)
+        if not result_content:
+            continue
+        yield parse_result(result_content, players)
 
 
-def read_result(path):
+def read_file(path):
     with open(path) as f:
         return f.read()
 
 
-def parse_result(content):
+def parse_config(content):
     data = json.loads(content)
-    return dict(first=get_record(data, 0), second=get_record(data, 1), seed=data['seed'])
+    return {get_player_name(v): n for n, v in enumerate(data['players'])}
+
+
+def get_player_name(data):
+    if isinstance(data, str):
+        return data
+    if isinstance(data, dict):
+        if 'Empty' in data:
+            return 'Empty'
+        if 'Tcp' in data:
+            return 'Tcp_%s' % str(data['Tcp']['port'])[:3]
+    raise RuntimeError('Invalid player data: %s' % data)
+
+
+def parse_result(content, players):
+    data = json.loads(content)
+    results = {name: get_record(data, index) for name, index in players.items()}
+    return dict(results=results, seed=data['seed'])
 
 
 def get_record(data, index):
     return dict(
         crashed=data['players'][index]['crashed'],
         score=data['results'][index],
-        place=next(n for n, v in enumerate(sorted(data['results'], reverse=True)) if v == data['results'][index]) + 1,
+        position=index,
     )
 
 
---
+++ scripts/run.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import time
+import pathlib
+import os.path
+import datetime
+import subprocess
+import click
+import copy
+import os
+
+
+@click.command()
+@click.option('--opponent_type', default='Quickstart', type=click.Choice((
+    'Tcp',
+    'Quickstart',
+    'Empty',
+)))
+@click.option('--game_type', default='2x1', type=click.Choice((
+    '2x1',
+    '2x2',
+    '2x2+',
+)))
+@click.option('--player_port', default=31010, type=int)
+@click.option('--other_port', default=32010, type=int)
+@click.option('--max_runs', default=2**64 - 1, type=int)
+@click.option('--prefix', default='default')
+@click.option('--aicup_bin', required=True, type=str)
+@click.option('--config_template_path', required=True, type=str)
+@click.option('--output_path', default=os.path.join(os.getcwd(), 'results'), type=str)
+def main(opponent_type, game_type, player_port, other_port, max_runs, prefix, aicup_bin, config_template_path, output_path):
+    config_template = read_json(config_template_path)
+    session = '%s.%s.%s.%s' % (prefix, game_type, player_port, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    session_path = os.path.join(output_path, opponent_type, game_type, session)
+    os.makedirs(session_path, exist_ok=False)
+    for number in range(max_runs):
+        run_game(
+            number=number,
+            session_path=session_path,
+            opponent_type=opponent_type,
+            game_type=game_type,
+            player_port=player_port,
+            other_port=other_port,
+            aicup_bin=aicup_bin,
+            config_template=config_template,
+        )
+
+
+def run_game(number, session_path, opponent_type, game_type, player_port, other_port, aicup_bin, config_template):
+    game = '%s.%s' % (number, int(time.time() * 1e6))
+    game_path = os.path.join(session_path, game)
+    os.makedirs(game_path, exist_ok=False)
+    config_path = os.path.join(game_path, 'config.json')
+    result_path = os.path.join(game_path, 'result.json')
+    swap = number % 2 == 1
+    config = generate_config(
+        config_template=config_template,
+        opponent_type=opponent_type,
+        game_type=game_type,
+        player_port=player_port,
+        other_port=other_port,
+        swap=swap,
+    )
+    write_json(data=config, path=config_path)
+    player_names = [other_port, player_port] if swap else [player_port, other_port]
+    print(number, game_path, player_names)
+    run_aicup(
+        aicup_bin=aicup_bin,
+        config_path=config_path,
+        result_path=result_path,
+        player_names=player_names,
+    )
+
+
+def run_aicup(aicup_bin, config_path, result_path, player_names):
+    subprocess.run([
+        aicup_bin,
+        '--batch-mode',
+        '--config', config_path,
+        '--save-results', result_path,
+        '--player-names', *[str(v) for v in player_names],
+    ])
+
+
+def read_json(path):
+    with open(path) as stream:
+        return json.load(stream)
+
+
+def write_json(data, path):
+    with open(path, 'w') as stream:
+        json.dump(data, stream, indent=4)
+
+
+def generate_config(config_template, opponent_type, game_type, player_port, other_port, swap):
+    player = make_player(player_port)
+    opponent = generate_opponent(opponent_type=opponent_type, port=other_port)
+    level, properties = generate_game(game_type, template=config_template['options_preset']['Custom']['properties'])
+    return {
+        'seed': None,
+        'options_preset': {
+            'Custom': {
+                'level': level,
+                'properties': properties,
+            }
+        },
+        'players': [opponent, player] if swap else [player, opponent]
+    }
+
+
+def make_player(port):
+    return {
+        'Tcp': {
+            'host': None,
+            'port': port,
+            'accept_timeout': None,
+            'timeout': None,
+            'token': None,
+        }
+    }
+
+
+def generate_opponent(opponent_type, port):
+    if opponent_type == 'Tcp':
+        return make_player(port)
+    if opponent_type == 'Quickstart':
+        return 'Quickstart'
+    if opponent_type == 'Empty':
+        return {'Empty': None}
+    raise RuntimeError('Invalid opponent_type: %s' % opponent_type)
+
+
+def generate_game(game_type, template):
+    if game_type == '2x1':
+        return 'Simple', make_properties(team_size=1, template=template)
+    if game_type == '2x2':
+        return 'Simple', make_properties(team_size=2, template=template)
+    if game_type == '2x2+':
+        return 'Complex', make_properties(team_size=2, template=template)
+    raise RuntimeError('Invalid game_type: %s' % game_type)
+
+
+def make_properties(team_size, template):
+    data = copy.deepcopy(template)
+    data['team_size'] = team_size
+    return data
+
+
+if __name__ == "__main__":
+    main()
