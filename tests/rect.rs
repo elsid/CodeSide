c003091af7d4ac1c553fe061755548e1e59cc2a5
elsid <elsid.mail@gmail.com>
2020-01-02 20:26:41 +0100

    More generic rect tests

--- tests/rect.rs
+++ tests/rect.rs
@@ -4,97 +4,33 @@ use aicup2019::my_strategy::{
 };
 
 #[test]
-fn test_has_intersection_with_line_horizontal() {
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-2.0, 0.0), Vec2::new(2.0, 0.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(2.0, 0.0), Vec2::new(-2.0, 0.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(2.0, 0.5), Vec2::new(-2.0, 0.5)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(2.0, 1.0), Vec2::new(-2.0, 1.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(2.0, 2.0), Vec2::new(-2.0, 2.0)),
-        false
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-3.0, 0.0), Vec2::new(-2.0, 0.0)),
-        false
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(2.0, 0.0), Vec2::new(3.0, 0.0)),
-        false
-    );
+fn test_rect_has_intersection_with_line_cross_rect() {
+    let rect = Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0));
+    for i in -10 .. 11 {
+        let angle = i as f64 * std::f64::consts::PI / 10.0;
+        let direction = Vec2::i().rotated(angle);
+        let a = -direction * 10.0;
+        let b = direction * 10.0;
+        assert!(rect.has_intersection_with_line(a, b), "{} {:?} {:?}", angle, a, b);
+    }
 }
 
 #[test]
-fn test_has_intersection_with_line_vertical() {
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(0.0, -2.0), Vec2::new(0.0, 2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(0.0, 2.0), Vec2::new(0.0, -2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(0.5, 2.0), Vec2::new(0.5, -2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(1.0, 2.0), Vec2::new(1.0, -2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(2.0, 2.0), Vec2::new(2.0, -2.0)),
-        false
-    );
-}
-
-#[test]
-fn test_has_intersection_with_line_left_bottom_to_top_right() {
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-2.0, -2.0), Vec2::new(2.0, 2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-3.0, -2.0), Vec2::new(2.0, 2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-2.0, -3.0), Vec2::new(2.0, 2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-2.0, -2.0), Vec2::new(2.0, 3.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-2.0, -2.0), Vec2::new(3.0, 2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-1.0, -2.0), Vec2::new(3.0, 2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(-2.0, -1.0), Vec2::new(2.0, 3.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(0.0, -2.0), Vec2::new(4.0, 2.0)),
-        true
-    );
-    assert_eq!(
-        Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0)).has_intersection_with_line(Vec2::new(1.0, -2.0), Vec2::new(5.0, 4.0)),
-        false
-    );
+fn test_rect_has_intersection_with_line_outside() {
+    let shifts = [
+        Vec2::new(-20.0, 0.0),
+        Vec2::new(20.0, 0.0),
+        Vec2::new(0.0, -20.0),
+        Vec2::new(0.0, 20.0),
+    ];
+    let rect = Rect::new(Vec2::new(0.0, 0.0), Vec2::new(1.0, 1.0));
+    for &shift in &shifts {
+        for i in -10 .. 11 {
+            let angle = i as f64 * std::f64::consts::PI / 10.0;
+            let direction = Vec2::i().rotated(angle);
+            let a = shift - direction * 10.0;
+            let b = shift + direction * 10.0;
+            assert!(!rect.has_intersection_with_line(a, b), "{} {:?} {:?}", angle, a, b);
+        }
+    }
 }
